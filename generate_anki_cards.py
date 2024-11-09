bc7a15a4503f0372cd74ce3762fb67c6a4a2f6be
Sitrosi <sitrosi@proton.me>
2024-11-09 23:08:51 +0200

    Add sound and image modeling

--- generate_anki_cards.py
+++ generate_anki_cards.py
@@ -2,7 +2,9 @@ import genanki
 
 base_model_idx = 2**31
 model_ids = {
-    'simple_text': base_model_idx
+    'simple_text': base_model_idx,
+    'simple_image': base_model_idx + 1,
+    'simple_sound': base_model_idx + 2,
 }
 
 text_model = genanki.Model(
@@ -21,13 +23,47 @@ text_model = genanki.Model(
     ]
 )
 
+image_model = genanki.Model(
+    model_ids['simple_image'],
+    'Basic I&A',
+    fields=[
+        {'name': 'Front'},
+        {'name': 'Back'},
+        {'name': 'Image'}
+    ],
+    templates=[
+        {
+            'name': 'Image Card',
+            'qfmt': '{{Front}}<br>{{Image}}',
+            'afmt': '{{FrontSide}}<hr id="answer">{{Back}}',
+        }
+    ]
+)
+
+sound_model = genanki.Model(
+    model_ids['simple_sound'],
+    'Basic S&A',
+    fields=[
+        {'name': 'Front'},
+        {'name': 'Back'},
+        {'name': 'Sound'}
+    ],
+    templates=[
+        {
+            'name': 'Sound Card',
+            'qfmt': '{{Front}}<br>{{Sound}}',
+            'afmt': '{{FrontSide}}<hr id="answer">{{Back}}',
+        },
+    ],
+)
+
 base_deck_idx = 2**30
 deck = genanki.Deck(
     base_deck_idx,
     'Minimal Q&A Deck'
 )
 
-mp = 'resources/basics'
+media_path = 'resources/basics'
 questions = [
     {
         'model': text_model,
@@ -44,7 +80,15 @@ questions = [
     {
         'model': text_model,
         'fields': ['What is your favourite colour?', 'Blue']
-    }
+    },
+    {
+        'model': image_model,
+        'fields': ['What animal is this?', 'Dog', '<img src="Bark.jpg">']
+    },
+    {
+        'model': sound_model,
+        'fields': ['What animal makes this sound?', 'Dog', '[sound:Bark.ogg]']
+    },
 ]
 
 for q in questions:
@@ -55,4 +99,7 @@ for q in questions:
     deck.add_note(note)
 
 package = genanki.Package(deck)
-package.write_to_file("Basic_QA_Deck.apkg")
+package.media_files = [f'{media_path}/Bark.jpg', f'{media_path}/Bark.ogg']
+package.write_to_file('Basic_QA_Deck.apkg')
+
+print("Created Basic_QA_Deck.apkg with media in subdirectories.")
